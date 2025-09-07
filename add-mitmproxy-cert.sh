0128744e6b63ee815db67315bb4551fb8933219b
David Dusa <david.dusa.hu@gmail.com>
2025-09-07 18:25:45 +0200

    add-mitmproxy-cert.sh: copy mitmproxyuser CA cert to OS cert store

--- add-mitmproxy-cert.sh
+++ add-mitmproxy-cert.sh
@@ -1,3 +1,4 @@
-sudo mkdir /usr/local/share/ca-certificates/extra
-sudo cp mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt
+sudo mkdir -p /usr/local/share/ca-certificates/extra
+# sudo cp mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt
+sudo cp /home/mitmproxyuser/.mitmproxy/mitmproxy-ca-cert.pem /usr/local/share/ca-certificates/extra/mitmproxy-ca-cert.crt
 sudo update-ca-certificates --fresh
